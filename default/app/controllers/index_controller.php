5a7ed17b258cb7c76174728a0f0ce6dde0022e6b
César Caballero <webmaster@ccaballero.com>
2012-07-23 13:54:11 +0200

    All site

---
+++ default/app/controllers/index_controller.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Controller por defecto si no se usa el routes
+ * 
+ */
+class IndexController extends AppController {
+	
+	
+	public function index()	{
+		
+	}
+	
+	public function caracteristicas() {
+	
+	}	
+	
+	
+	public function descargas() {
+		
+	}
+	
+	public function documentacion() {
+		
+	}
+	
+	public function irc() {
+		
+	}
+
+	
+	
+}
---
+++ default/app/views/pages/main/index.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Project: Kumbiaphp - Main Site
+File: index.php
+Created: 27/06/2012 14:02:11
+*/
+
+?>
